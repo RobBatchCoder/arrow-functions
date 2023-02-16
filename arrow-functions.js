a3c95696fbd7571962490333e64b85013d9b7c66
RobBatchCoder <robertbatchelor909@gmail.com>
2023-02-16 14:00:53 +0000

    complete arrow function tutorial

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,34 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers1(a,b){
+    return a + b;
+}
+let sum1 = addTwoNumbers1(4,5);
+console.log(sum1);
+
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers2(4,5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a,b) =>  a + b;
+console.log(addTwoNumbers3(4,5));
 
 // Implicit Returns
+const saySomething = (message) => console.log(message);
+saySomething("Hello");
+
+const sayHello = () => console.log("Hello");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        Hey, how are you!
+    </p>
+    `)
+console.log(returnMultipleLines());
